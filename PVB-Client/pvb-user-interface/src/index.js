0499a98ec3a44b2948c9ebb27c1b409e03f78a7a
Anjana <adeegoda@wiley.com>
2024-01-30 10:10:14 +0530

    Added Imports and semantic-ui dependancies

--- PVB-Client/pvb-user-interface/src/index.js
+++ PVB-Client/pvb-user-interface/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import _ from 'lodash';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
+import {
+  Button,
+  Card,
+  Divider,
+  Image,
+  Placeholder,
+} from 'semantic-ui-react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const pvb_main = ReactDOM.createRoot(document.getElementById('root'));
+pvb_main.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
