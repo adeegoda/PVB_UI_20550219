5b0f10002c158f77c477d57d2f82fc6de577cd8f
Anjana <adeegoda@wiley.com>
2024-02-11 11:48:08 +0530

    Updated resources and coverpage

---
+++ PVB-Client/pvb-user-interface/src/Pages/Cover.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import logo from '../Resources/logo.svg';
+import '../Resources/coverPage.css';
+
+const PVB_CoverUI = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    );
+}
+
+export default PVB_CoverUI;
--- PVB-Client/pvb-user-interface/src/index.js
+++ PVB-Client/pvb-user-interface/src/index.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CoverUI from './Pages/Cover';
+import EBallotUI from './Pages/EBallot';
 
 const PVB_MainUI = () => {
   return (
-    <>Test Main Page</>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={CoverUI} />
+          <Route path="/votingUI" component={EBallotUI} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
