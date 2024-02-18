c3fa97ade894647c82f8a171d4a715f080eb57f7
Anjana <adeegoda@wiley.com>
2024-02-18 15:14:54 +0530

    added identification page styles

--- Paperless-Voting-Booth/src/Pages/IdVerification.js
+++ Paperless-Voting-Booth/src/Pages/IdVerification.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import '../Resources/coverPage.css';
+import '../Resources/idVerificationPage.css';
 import { Link } from 'react-router-dom';
 
 
 const PVB_IdVerificationUI = () => {
     return (
-        <div>
-            <h1>
-                    <p>
-                        චන්ද ප්‍රකාශය ආරම්භ කරන්න<br />
-                        Start Casting Vote<br />
-                        வாக்களிக்கத் தொடங்குங்கள்<br />
-                    </p>
-            </h1>
+        <div className="container">
+            <h2>
+                <p>
+                    ජාතික හැදුනුම්ත් අංකය තහවුරු කිරීම <br />
+                    Verify National Identification Card Number<br />
+                    தேசிய அடையாள அட்டை எண்ணைச் சரிபார்க்கவும்<br />
+                </p>
+            </h2>
+            <div className="centered">
+                <div className="nic_input_field">
+                    <input name="nic_number" placeholder="Enter NIC Number" />
+                </div>
+                <div className="nic_verification_button">
+                    <button name="verify_nic_number">
+                        තහවුරු කරන්න <br />
+                        Verify<br />
+                        சரிபார்க்கவும்<br />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
