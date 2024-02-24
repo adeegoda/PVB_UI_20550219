a15f41314f311a455a0aab4f83aebd7492dda505
Anjana <adeegoda@wiley.com>
2024-02-24 21:17:51 +0530

    added two more parties for demonstration

--- Paperless-Voting-Booth/src/Pages/EBallot.js
+++ Paperless-Voting-Booth/src/Pages/EBallot.js
@@ -27,8 +27,22 @@ const cards = [
         party_code: 'SLPP',
         avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Sri_Lanka_Podujana_Peramuna_election_symbol.svg/125px-Sri_Lanka_Podujana_Peramuna_election_symbol.svg.png',
         header1: "ශ්‍රී ලංකා පොදුජන පෙරමුණ",
-        header2: "Sri Lanka Podujana Peramuna ",
+        header2: "Sri Lanka Podujana Peramuna",
         header3: "இலங்கை பொதுஜன முன்னணி",
+    },
+    {
+        party_code: 'FPA',
+        avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ef/Logo_of_the_Freedom_People%27s_Alliance.png/180px-Logo_of_the_Freedom_People%27s_Alliance.png',
+        header1: "නිදහස් ජනතා සන්ධානය",
+        header2: "Freedom People's Alliance",
+        header3: "சுதந்திர மக்கள் கூட்டணி",
+    },
+    {
+        party_code: 'DTNA',
+        avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Election_Symbol_Demacratic_Tamila_Natinal_Aliance_Sri_Lanka.png/150px-Election_Symbol_Demacratic_Tamila_Natinal_Aliance_Sri_Lanka.png',
+        header1: "දෙමළ ජාතික සන්ධානය",
+        header2: "Democratic Tamil National Alliance",
+        header3: "தமிழ்த் தேசியக் கூட்டமைப்பு",
     },
 ];
 
