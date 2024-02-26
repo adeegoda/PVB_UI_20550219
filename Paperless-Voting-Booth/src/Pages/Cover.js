e4e62443a431187d5fbb1046bbdb23e28f830b0f
Anjana <adeegoda@wiley.com>
2024-02-26 11:07:32 +0530

    Added Election details display UI and created spserate classes for API operators

--- Paperless-Voting-Booth/src/Pages/Cover.js
+++ Paperless-Voting-Booth/src/Pages/Cover.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Resources/coverPage.css';
 import { Link } from 'react-router-dom';
+import { fetchElectionDetails } from '../APIOperators/ElectionDetailsAPI';
 
 
 const PVB_CoverUI = () => {
+    const [electionDetails, setElectionDetails] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetchElectionDetails();
+            setElectionDetails(data);
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
+            <div className="container">
+                <h2>
+                    {electionDetails.map(election => (
+                        <React.Fragment key={election._id}>
+                            {election.election_name_sinhala} | {election.election_name_english} | {election.election_name_tamil} - {election.election_year}
+                        </React.Fragment>
+                    ))}
+                </h2>
+            </div>
             <h1>
                 <Link to='/otpVerificationUI' class="coverPage">
                     <p>
@@ -16,6 +36,7 @@ const PVB_CoverUI = () => {
                 </Link>
             </h1>
         </div>
+
     );
 }
 
