516c95ea32984f1dfdf204c6fa2b23e7426617c7
adeegoda <anjideegoda@gmail.com>
2024-05-16 12:55:36 +0530

    vivaFeedbackImplementation_Frontend: Optimized Dashboard View

--- Paperless-Voting-Booth/src/Pages/ElectionDashboard.js
+++ Paperless-Voting-Booth/src/Pages/ElectionDashboard.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Bar } from 'react-chartjs-2';
+import { FetchElectionDetails } from '../APIOperators/ElectionDetailsAPI';
 
 const Dashboard = () => {
+  const [electionDetails, setElectionDetails] = useState([]);
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios('http://localhost:4000/pvb-api/votes-per-party');
-      setData(result.data);
-    };
+  const fetchData = async () => {
+    const data = await FetchElectionDetails();
+    setElectionDetails(data);
+    const result = await axios('http://localhost:4000/pvb-api/votes-per-party');
+    setData(result.data);
+  };
 
+  useEffect(() => {
     fetchData();
+    const voteRefreshIntervalId = setInterval(fetchData, 5000);
+
+    const dateTimeIntervalId = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(voteRefreshIntervalId);
+      clearInterval(dateTimeIntervalId);
+    };
   }, []);
 
   const chartData = {
@@ -28,6 +42,29 @@ const Dashboard = () => {
       }
     ]
   };
-  return <Bar data={chartData} />;
+
+  if (!chartData) {
+    return <p style={{ color: 'red', fontSize: '20px' }}>Loading chart data...</p>;
+  }
+
+  return (<div>
+    <div className="centered">
+      <h1>
+        {electionDetails.map(election => (
+          <React.Fragment key={election._id}>
+            {election.election_name_sinhala} | {election.election_name_english} | {election.election_name_tamil} - {election.election_year}
+          </React.Fragment>
+        ))}
+      </h1>
+      <h2>
+        {currentDateTime.toLocaleString()}
+      </h2>
+    </div>
+    <div className="vote_chart" style={{ width: '700px', height: '500px' }}>
+      <Bar data={chartData} />
+    </div>
+  </div>
+  );
 };
+
 export default Dashboard;
