b7de0956ff36460868ab1ae8e7288c3815c72754
Anjana Deegoda <87065519+adeegoda@users.noreply.github.com>
2024-12-30 23:08:16 +0530

    Enhance OTP generation API to include authorization token and improve error handling for NIC validation

--- PVB-Frontend/src/APIOperators/OTPGenerationAPI.js
+++ PVB-Frontend/src/APIOperators/OTPGenerationAPI.js
@@ -1,31 +1,34 @@
 export const generateOTP = async (nic, setNicErrorMessage, setOtpErrorMessage) => {
-    if (nic !== '') {
-        if (!/^[0-9]{9}V$/.test(nic)) {
-            setNicErrorMessage('වලංගු නොවන NIC ආකෘතියකි | Invalid NIC format | தவறான NIC வடிவம்');
-            return;
-        }
+  if (nic !== '') {
+    if (!/^[0-9]{9}V$/.test(nic)) {
+      setNicErrorMessage('වලංගු නොවන NIC ආකෘතියකි | Invalid NIC format | தவறான NIC வடிவம்');
+      return;
+    }
+  } else {
+    setNicErrorMessage('NIC අංකය ඇතුලත් කර නොමැත | NIC field is empty | NIC புலம் காலியாக உள்ளது');
+    return;
+  }
+
+  try {
+    const token = localStorage.getItem('token');
+    const response = await fetch("http://localhost:4000/pvb-api/generate-otp", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`, // Include the authorization token
+        'voter-nic': nic,
+      },
+      body: JSON.stringify({ nic: nic })
+    });
+    const data = await response.json();
+    if (response.status === 201) {
+      return data.otp;
     } else {
-        setNicErrorMessage('NIC අංකය ඇතුලත් කර නොමැත | NIC field is empty | NIC புலம் காலியாக உள்ளது');
-        return;
+      setOtpErrorMessage(data.message);
+      return;
     }
-    try {
-        const response = await fetch("http://localhost:4000/pvb-api/generate-otp", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'voter-nic': nic,
-            },
-            body: JSON.stringify({ nic: nic })
-        });
-        const data = await response.json();
-        if (response.status === 201) {
-            return data.otp;
-        } else {
-            setOtpErrorMessage(data.message)
-            return;
-        }
-    } catch (error) {
-        setOtpErrorMessage('Error generating OTP:', error)
-        console.error('Error generating OTP:', error);
-    }
+  } catch (error) {
+    setOtpErrorMessage('Error generating OTP');
+    console.error('Error generating OTP:', error);
+  }
 };
